d6057d7d56a67b3572b1aa7d23366beca7cebd26
Дмитрий Сердюков <dimaserd84@gmail.com>
2017-03-07 00:23:52 +0300

    лдроп

--- Runtasker/Controllers/LandingController.cs
+++ Runtasker/Controllers/LandingController.cs
@@ -7,10 +7,7 @@ namespace Runtasker.Controllers
         // GET: Landing
         public ActionResult Index()
         {
-            if(!User.IsInRole("Admin"))
-            {
-                return RedirectToAction("Index", "Home");
-            }
+            
             return View();
         }
 

5d05670ca73bfe7f5d0d76f68fa158935b2337d7
Дмитрий Сердюков <dimaserd84@gmail.com>
2017-05-10 00:11:16 +0300

    лор

--- Runtasker/App_Start/Startup.Auth.cs
+++ Runtasker/App_Start/Startup.Auth.cs
@@ -5,7 +5,6 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
-using Runtasker.Logic;
 using Microsoft.Owin.Security.Twitter;
 using KatanaContrib.Security.VK;
 using KatanaContrib.Security.Instagram;

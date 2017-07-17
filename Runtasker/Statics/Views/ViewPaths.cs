dbf528cc298d6a9c7681ac726f412aa9f2bde74c
Дмитрий Сердюков <dimaserd84@gmail.com>
2017-07-17 09:53:25 +0300

    f

---
+++ Runtasker/Statics/Views/ViewPaths.cs
@@ -0,0 +1,42 @@
+﻿namespace Runtasker.Statics.Views
+{
+    /// <summary>
+    /// Содержит статические пути к мастер представлениям.
+    /// </summary>
+    public static class ViewPaths
+    {
+        private const string LayoutsDirectory = "~/Views/Layouts";
+
+        public static string ModalLayoutPath
+        {
+            get
+            {
+                return $"{LayoutsDirectory}/_ModalLayout.cshtml";
+            }
+        }
+
+        public static string NewLayoutPath
+        {
+            get
+            {
+                return $"{LayoutsDirectory}/_NewLayout.cshtml";
+            }
+        }
+
+        public static string NewAdminLayoutPath
+        {
+            get
+            {
+                return $"{LayoutsDirectory}/_AdminLayout.cshtml";
+            }
+        }
+
+        public static string EmptyLayoutPath
+        {
+            get
+            {
+                return $"{LayoutsDirectory}/_EmptyLayout.cshtml";
+            }
+        }
+    }
+}

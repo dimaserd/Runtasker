6f62788c20ce11f879d4100bd01883b9e7ac4a7c
dimaserd <dimaserd84@84gmail.com>
2017-09-21 20:16:41 +0300

    jhgf

---
+++ Extensions/Attributes/TooltipAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.Attributes
+{
+    public class TooltipAttribute : CustomResourceAttribute
+    {
+        public TooltipAttribute(string text) : base(text)
+        {
+
+        }
+
+        public TooltipAttribute(Type resourceType, string resourceName) : base(resourceType, resourceName)
+        {
+
+        }
+
+        public string TooltipText
+        {
+            get
+            {
+                return Text;
+            }
+        }
+    }
+}
